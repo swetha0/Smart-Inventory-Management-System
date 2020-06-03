b928c1fc25f20dd9d3500e906a7094900c0e5906
swetha0 <gaali.swetha@gmail.com>
2020-06-03 15:24:39 -0400

    display supliers done

--- src/dao/SupplierDAO.java
+++ src/dao/SupplierDAO.java
@@ -14,7 +14,7 @@ public class SupplierDAO implements SupplierDaoInterface {
 		Connection con=ConnectionManager.getConnection();
 		PreparedStatement pstmt=con.prepareStatement("select * from suppliersProducts");
 		ResultSet rs=pstmt.executeQuery();
-		System.out.printf("ID     Name                 ProductId          ProductName               Category            Brand            Quantity         Price %n");
+		System.out.printf("ID     Name                 ProductId    ProductName            Category                   Brand              Quantity         Price %n");
 		System.out.println("____________________________________________________________________________________________________________________________________________");
 		while(rs.next()) {
 			System.out.printf("%-6d %-20s %-10d %-25s %-25s %-20s %-15d %-15d %n",rs.getInt(1),rs.getString(2),rs.getInt(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getInt(7),rs.getInt(8));
